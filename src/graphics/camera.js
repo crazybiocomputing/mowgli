21b1fa091e3132842477d5f3ef4197160fc37622
jeesay <jjeesay@gmail.com>
2015-02-03 15:32:08 +0100

    Create camera.js

---
+++ src/graphics/camera.js
@@ -0,0 +1,33 @@
+/*
+ *  mowgli: Molecule WebGL Viewer in JavaScript, html5, css3, and WebGL
+ *  Copyright (C) 2015  Jean-Christophe Taveau.
+ *
+ *  This file is part of mowgli
+ *
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with dotplot_wgl.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Authors:
+ * Jean-Christophe Taveau
+ */
+ 
+ "use strict"
+ 
+ function Camera() {
+    this.projMatrix = mat4.create();
+    this.viewMatrix = mat4.create();
+    this.fovy = 45.0*Math.PI/180.0;
+  }
+  
+  
