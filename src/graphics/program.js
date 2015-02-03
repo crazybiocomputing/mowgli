b08df169d45c99702654cde8e789b71de1814956
jeesay <jjeesay@gmail.com>
2015-02-03 15:15:14 +0100

    Update program.js

--- src/graphics/program.js
+++ src/graphics/program.js
@@ -22,3 +22,139 @@
  * Jean-Christophe Taveau
  */
  
+var Program = function(context,name) {
+  this.ctx = context;
+  this.name = name;
+  this.vertex_shader   = null;
+  this.fragment_shader = null;
+  this.shaderProgram = 0;
+  this.uniforms={};
+}
+
+Program.prototype.getID=function() {
+  return this.shaderProgram;
+}
+
+Program.prototype.load=function(type,name) {
+  // TODO
+  if (type =='vertex')
+    // this.vertex_shader = ShaderFactory.get(type,name);
+    console.log('vertex');
+  else if (type =='fragment')
+    // this.fragment_shader = ShaderFactory.get(type,name);
+    console.log('fragment');
+  else
+    alert('Unknown shader type');
+}
+
+Program.prototype.loadHTTP=function(type,name) {
+  // From http://www.html5rocks.com/en/tutorials/file/xhr2/
+  // XMLHttpRequest()
+  // TODO
+  var url = 'shaders/'+name+'.'+type;
+  var req = new XMLHttpRequest();
+  req.open("GET", url, true);
+  req.responseType = 'text';
+  req.onload = function() {
+    if (this.status === 200) {
+      this.fragment_shader = this._compile(gl.FRAGMENT_SHADER,this.response);
+    }
+  }
+  req.send();
+}
+
+/* From Learning WEBGL */
+Program.prototype.loadDOM=function(type,name) {
+  var gl = this.ctx;
+
+  var shaderScript = document.getElementById(name);
+  if (!shaderScript) {
+    return null;
+  }
+
+  var str = "";
+  var k = shaderScript.firstChild;
+  while (k) {
+    if (k.nodeType == 3) {
+      str += k.textContent;
+    }
+    k = k.nextSibling;
+  }
+
+  if (shaderScript.type == "x-shader/x-fragment") {
+    this.fragment_shader = this._compile(gl.FRAGMENT_SHADER,str);
+  } else if (shaderScript.type == "x-shader/x-vertex") {
+    this.vertex_shader = this._compile(gl.VERTEX_SHADER,str);
+  } else {
+    return null;
+  }
+}
+
+
+Program.prototype.link=function() {
+  var gl = this.ctx;
+  this.shaderProgram = gl.createProgram();
+  gl.attachShader(this.shaderProgram, this.vertex_shader);
+  gl.attachShader(this.shaderProgram, this.fragment_shader);
+  gl.linkProgram(this.shaderProgram);
+
+  if (!gl.getProgramParameter(this.shaderProgram, gl.LINK_STATUS)) {
+    alert("Could not initialise shaders");
+  }
+}
+
+Program.prototype.use=function() {
+  var gl = this.ctx;
+  gl.useProgram(this.shaderProgram);
+}
+
+
+Program.prototype.setUniforms=function(uniform_list) {
+  this.uniforms = uniform_list;
+
+  var gl = this.ctx;
+  for (var i in this.uniforms) {
+    console.log(i+' ' +this.uniforms[i]);
+    this.uniforms[i].location = gl.getUniformLocation(this.getID(), i);
+    switch (this.uniforms[i].type) {
+    case 'f':
+      gl.uniform1f(this.uniforms[i].location, this.uniforms[i].value);
+      break;
+    case 'm':
+      break;
+    case 't':
+      break;
+    case 'v':
+      break;
+    default:
+    }
+  }
+}
+
+Program.prototype.setUniform1f=function(name,value) {
+  var gl = this.ctx;
+  // TODO
+}
+
+Program.prototype.setUniform2f=function(name,value1, value2) {
+  var gl = this.ctx;
+
+  this.uniforms[name]=gl.getUniformLocation(this.shaderProgram, name);
+  gl.uniform2f(uniforms[name], value1, value2);
+}
+
+// Private
+Program.prototype._compile=function(type,text) {
+  var gl = this.ctx;
+  var shader = gl.createShader(type);        
+  gl.shaderSource(shader, text);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(gl.getShaderInfoLog(shader));
+    return null;
+  }
+
+  return shader;
+}
+
