7977a7ac061c8fcd2a2b32c9a3768fdce2b90543
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2016-03-07 10:40:44 +0100

    Put doc in gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,14 +17,28 @@ module.exports = function(grunt) {
         src: ['src/**/*.js'],
         dest: 'dist/<%= pkg.name %>.js'
       }
+    },
+    jsdoc : {
+        dist : {
+            src: ['src/**/*.js','src/**/README.md'],
+            options: {
+                configure: 'doc/jsdoc/config.json',
+                destination: 'gh-pages/doc'
+            }
+        }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-jsdoc');
+  
 
   // Default task(s).
   grunt.registerTask('default', ['concat','uglify']);
+
+  // this would be run by typing "grunt doc" on the command line
+  grunt.registerTask('doc', ['jsdoc']);
 
 };
