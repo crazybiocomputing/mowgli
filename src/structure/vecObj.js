8b1823c79e981835b13f6f9f5e330554c43c6954
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2016-04-13 14:24:49 +0200

    Add inheritance

--- src/structure/vecObj.js
+++ src/structure/vecObj.js
@@ -4,14 +4,14 @@
  *
  *  This file is part of mowgli
  *
- * This program is free software: you can redistribute it and/or modify it 
- * under the terms of the GNU General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or 
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
@@ -22,8 +22,7 @@
  * Jean-Christophe Taveau
  */
 
- 
-"use strict";
+'use strict';
 
 /**
  * 3D Vectorial Object
@@ -40,3 +39,4 @@ function VecObj() {
     // TODO Is it redundant with the Shape class?????
 }
 
+VecObj.prototype = Object.create(Structure.prototype);
